9bfda8d9a10a76b49967984fa7e6bce5299921b0
Steven Torrence <storrence88@gmail.com>
2019-02-21 20:16:33 -0600

    Add strong params to the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
