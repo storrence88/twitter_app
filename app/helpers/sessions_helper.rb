7961be7e40cd3c03782ff25eacb5ebe877d94948
Steven Torrence <storrence88@gmail.com>
2019-02-26 18:56:40 -0600

    Prevent any logged in user from editing another user's profile

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,14 +3,19 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-
+  
   # Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-
+  
+  # Returns true if the given user is the current user
+  def current_user?(user)
+    user == current_user
+  end
+  
   # Return the current logged in user (if any)
   def current_user
     if (user_id = session[:user_id])
